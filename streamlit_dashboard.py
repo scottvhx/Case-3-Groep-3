ae7e1c2bd93765c3d01c9987ab4c2bc0ee87ef73
scottvhx <a.s.vhagen@outlook.com>
2024-03-18 13:47:43 +0100

    Steamlist_dashboard update

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -2,60 +2,9 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import data_cleaning
 
 #data
-
-airport= pd.read_csv('airports-extended-clean.csv', sep = ';')
-flight1= pd.read_excel('1Flight 1.xlsx')
-flight2= pd.read_excel('1Flight 2.xlsx')
-flight3= pd.read_excel('1Flight 3.xlsx')
-flight4= pd.read_excel('1Flight 4.xlsx')
-flight5= pd.read_excel('1Flight 5.xlsx')
-flight6= pd.read_excel('1Flight 6.xlsx')
-flight7= pd.read_excel('1Flight 7.xlsx')
-flight31= pd.read_excel('30Flight 1.xlsx')
-flight32= pd.read_excel('30Flight 2.xlsx')
-flight33= pd.read_excel('30Flight 3.xlsx')
-flight34= pd.read_excel('30Flight 4.xlsx')
-flight35= pd.read_excel('30Flight 5.xlsx')
-flight36= pd.read_excel('30Flight 6.xlsx')
-flight37= pd.read_excel('30Flight 7.xlsx')
-schedule= pd.read_csv('schedule_airport.csv')
-
-# Controleren als NaN of - in de dataframe zit en anders veranderen naar de de nul
-
-schedule[["DL1", "IX1", "DL2", "IX2"]] = schedule[["DL1", "IX1", "DL2", "IX2"]].replace("-", "0")
-flight1['TRUE AIRSPEED (derived)'].fillna(0, inplace=True)
-flight2[['TRUE AIRSPEED (derived)', '[3d Latitude]', '[3d Longitude]', '[3d Altitude M]', '[3d Altitude Ft]', '[3d Heading]']] = flight3[['TRUE AIRSPEED (derived)', '[3d Latitude]', '[3d Longitude]', '[3d Altitude M]', '[3d Altitude Ft]', '[3d Heading]']].fillna(0)
-flight3[['TRUE AIRSPEED (derived)', '[3d Latitude]', '[3d Longitude]', '[3d Altitude M]', '[3d Altitude Ft]', '[3d Heading]']] = flight3[['TRUE AIRSPEED (derived)', '[3d Latitude]', '[3d Longitude]', '[3d Altitude M]', '[3d Altitude Ft]', '[3d Heading]']].fillna(0)
-flight4['TRUE AIRSPEED (derived)'].fillna(0, inplace=True)
-flight5['TRUE AIRSPEED (derived)'].fillna(0, inplace=True)
-flight6['TRUE AIRSPEED (derived)'].fillna(0, inplace=True)
-flight7[['TRUE AIRSPEED (derived)', '[3d Latitude]', '[3d Longitude]', '[3d Altitude M]', '[3d Altitude Ft]', '[3d Heading]']] = flight3[['TRUE AIRSPEED (derived)', '[3d Latitude]', '[3d Longitude]', '[3d Altitude M]', '[3d Altitude Ft]', '[3d Heading]']].fillna(0)
-flight32[['TRUE AIRSPEED (derived)', '[3d Latitude]', '[3d Longitude]', '[3d Altitude M]', '[3d Altitude Ft]', '[3d Heading]']] = flight3[['TRUE AIRSPEED (derived)', '[3d Latitude]', '[3d Longitude]', '[3d Altitude M]', '[3d Altitude Ft]', '[3d Heading]']].fillna(0)
-flight33[['TRUE AIRSPEED (derived)', '[3d Latitude]', '[3d Longitude]', '[3d Altitude M]', '[3d Altitude Ft]', '[3d Heading]']] = flight3[['TRUE AIRSPEED (derived)', '[3d Latitude]', '[3d Longitude]', '[3d Altitude M]', '[3d Altitude Ft]', '[3d Heading]']].fillna(0)
-flight34[['TRUE AIRSPEED (derived)', '[3d Latitude]', '[3d Longitude]', '[3d Altitude M]', '[3d Altitude Ft]', '[3d Heading]']] = flight3[['TRUE AIRSPEED (derived)', '[3d Latitude]', '[3d Longitude]', '[3d Altitude M]', '[3d Altitude Ft]', '[3d Heading]']].fillna(0)
-flight36[['TRUE AIRSPEED (derived)', '[3d Latitude]', '[3d Longitude]', '[3d Altitude M]', '[3d Altitude Ft]', '[3d Heading]']] = flight3[['TRUE AIRSPEED (derived)', '[3d Latitude]', '[3d Longitude]', '[3d Altitude M]', '[3d Altitude Ft]', '[3d Heading]']].fillna(0)
-
-
-# Controleren op duplicaten in de dataframe 
-scheduleclean= schedule.drop_duplicates()
-airportclean= airport.drop_duplicates()
-flight1clean= flight1.drop_duplicates()
-flight2clean= flight2.drop_duplicates()
-flight3clean= flight3.drop_duplicates()
-flight4clean= flight4.drop_duplicates()
-flight5clean= flight5.drop_duplicates()
-flight6clean= flight6.drop_duplicates()
-flight7clean= flight7.drop_duplicates()
-flight31clean= flight31.drop_duplicates()
-flight32clean= flight32.drop_duplicates()
-flight33clean= flight33.drop_duplicates()
-flight34clean= flight34.drop_duplicates()
-flight35clean= flight35.drop_duplicates()
-flight36clean= flight36.drop_duplicates()
-flight37clean= flight37.drop_duplicates()
-
 
 
 
@@ -95,18 +44,18 @@ with tab2:
     st.subheader('*Barplot*') 
 
     # Laad en reinig de gegevens
-    scheduleclean = pd.read_csv('schedule_airport.csv')
-    scheduleclean['STA_STD_ltc'] = pd.to_datetime(scheduleclean['STA_STD_ltc'])
-    scheduleclean['ATA_ATD_ltc'] = pd.to_datetime(scheduleclean['ATA_ATD_ltc'])
-    scheduleclean['Arrival_Status'] = scheduleclean['ATA_ATD_ltc'] - scheduleclean['STA_STD_ltc'] > pd.Timedelta(0)
-    scheduleclean['Departure_Status'] = scheduleclean['STA_STD_ltc'] - scheduleclean['ATA_ATD_ltc'] > pd.Timedelta(0)
+    #scheduleclean = pd.read_csv('schedule_airport.csv')
+    data_cleaning.scheduleclean['STA_STD_ltc'] = pd.to_datetime(data_cleaning.scheduleclean['STA_STD_ltc'])
+    data_cleaning.scheduleclean['ATA_ATD_ltc'] = pd.to_datetime(data_cleaning.scheduleclean['ATA_ATD_ltc'])
+    data_cleaning.scheduleclean['Arrival_Status'] = data_cleaning.scheduleclean['ATA_ATD_ltc'] - data_cleaning.scheduleclean['STA_STD_ltc'] > pd.Timedelta(0)
+    data_cleaning.scheduleclean['Departure_Status'] = data_cleaning.scheduleclean['STA_STD_ltc'] - data_cleaning.scheduleclean['ATA_ATD_ltc'] > pd.Timedelta(0)
 
     # Bereken aantallen voor de plot
-    total_arrival_flights = len(scheduleclean[scheduleclean['LSV'].str.contains('L')])
-    total_departure_flights = len(scheduleclean[scheduleclean['LSV'].str.contains('S')])
-    arrival_delay_count = scheduleclean[scheduleclean['Arrival_Status'] & scheduleclean['LSV'].str.contains('L')].shape[0]
+    total_arrival_flights = len(data_cleaning.scheduleclean[data_cleaning.scheduleclean['LSV'].str.contains('L')])
+    total_departure_flights = len(data_cleaning.scheduleclean[data_cleaning.scheduleclean['LSV'].str.contains('S')])
+    arrival_delay_count = data_cleaning.scheduleclean[data_cleaning.scheduleclean['Arrival_Status'] & data_cleaning.scheduleclean['LSV'].str.contains('L')].shape[0]
     arrival_ontime_count = total_arrival_flights - arrival_delay_count
-    departure_delay_count = scheduleclean[scheduleclean['Departure_Status'] & scheduleclean['LSV'].str.contains('S')].shape[0]
+    departure_delay_count = data_cleaning.scheduleclean[data_cleaning.scheduleclean['Departure_Status'] & data_cleaning.scheduleclean['LSV'].str.contains('S')].shape[0]
     departure_ontime_count = total_departure_flights - departure_delay_count
 
     # Set kleuren voor de balken
